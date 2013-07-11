a081854e21141136ee560e91fa4d45cc2a0eff5f
notandrewkaye <notandrewkaye@gmail.com>
2013-07-11 17:59:02 -0500

    added led status graphic

--- ofxBufferedSerial/serialTest/src/testApp.cpp
+++ ofxBufferedSerial/serialTest/src/testApp.cpp
@@ -3,6 +3,12 @@
 //--------------------------------------------------------------
 void testApp::setup(){
 
+    
+    
+    LED led;
+    localLEDs.assign(DEFAULT_NUM_LEDS,led);
+    remoteLEDs.assign(DEFAULT_NUM_LEDS,led);
+    
 }
 
 //--------------------------------------------------------------
@@ -12,11 +18,65 @@ void testApp::update(){
 
 //--------------------------------------------------------------
 void testApp::draw(){
+    
+    ofBackground(0);
+    int width  = ofGetWidth();
+    int height = ofGetHeight();
+    
+    int halfWidth  = width  / 2;
+    int halfHeight = height / 2;
+    
+    int xSpace = 5;
+    int ySpace = 5;
+    
+    int xOffset = xSpace;
+    int yOffset = ySpace;
+    
+    int xIncrement = LED::DEFAULT_WIDTH  + xSpace;
+    int yIncrement = LED::DEFAULT_HEIGHT + ySpace;
+
+    
+    for(size_t i = 0; i < DEFAULT_NUM_LEDS; ++i)
+    {
+        ofPushMatrix();
+        localLEDs[i].draw(xOffset, yOffset);
+        remoteLEDs[i].draw(xOffset, yOffset + halfHeight);
+        
+        xOffset += xIncrement;
+        if(xOffset + xIncrement > ofGetWidth())
+        {
+            yOffset += yIncrement;
+            xOffset = xSpace;
+        }
+        ofPopMatrix();
+    }
+    
+    ofSetColor(255);
+    ofLine(0,ofGetHeight()/2, ofGetWidth(), ofGetHeight()/2);
+    
+    ofDrawBitmapStringHighlight("Local Values", halfWidth-50, halfHeight-20, ofColor(80),ofColor(255,255,0));
+    ofDrawBitmapStringHighlight("Echoed Remote Values", halfWidth-75, height-20, ofColor(80),ofColor(255,255,0));
 
 }
 
 //--------------------------------------------------------------
 void testApp::keyPressed(int key){
+    if(key == 'r') {
+        // set random local led to randome color and send
+        size_t randomIndex = (size_t)ofRandom(DEFAULT_NUM_LEDS);
+        localLEDs[randomIndex].setRandomColor();
+        
+    } else if(key == ' ') {
+        // set all leds to random color and send
+        for(size_t i = 0; i < DEFAULT_NUM_LEDS; ++i) {
+            localLEDs[i].setRandomColor();
+        }
+    } else if(key == 'c') {
+        // set all leds to white
+        for(size_t i = 0; i < DEFAULT_NUM_LEDS; ++i) {
+            localLEDs[i].setColor(255);
+        }
+    }
 
 }
 
--- ofxBufferedSerial/serialTest/src/testApp.h
+++ ofxBufferedSerial/serialTest/src/testApp.h
@@ -2,6 +2,75 @@
 
 #include "ofMain.h"
 
+/*
+ 
+ B0     -> (uint8_t)  : The type of the byte array
+ : 0 : uint8_t  array
+ : 1 : uint16_t array
+ : 2 : uint32_t array
+ : 3 : int8_t   array
+ : 4 : int16_t  array
+ : 5 : int32_t  array
+ 
+ B1, B2     -> (uint16_t) : The length of the payload (up to 65536)
+ B3...BN    -> Payload bytes to be interpreted based on B0
+ BN+1,BN+2  -> Payload Checksum
+ */
+
+
+class LED
+{
+public:
+    LED(int width = DEFAULT_WIDTH,
+        int height = DEFAULT_HEIGHT,
+        const ofColor& color = ofColor(255,255,255,255)) :
+    _width(width),
+    _height(height),
+    _color(color)
+    {
+    }
+    
+    void setRandomColor()
+    {
+        unsigned char r = ofRandom(0,255);
+        unsigned char g = ofRandom(0,255);
+        unsigned char b = ofRandom(0,255);
+        unsigned char a = 255;
+        
+        _color.set(r,g,b,a);
+    }
+    
+    void setColor(const ofColor& color)
+    {
+        _color = color;
+    }
+    
+    void draw(int x, int y)
+    {
+        ofPushMatrix();
+        ofPushStyle();
+        ofTranslate(x,y);
+        
+        ofSetColor(_color);
+        ofRect(0,0,_width,_height);
+        
+        ofPopStyle();
+        ofPopMatrix();
+    }
+    
+    enum
+    {
+        DEFAULT_WIDTH  = 32,
+        DEFAULT_HEIGHT = 16
+    };
+    
+protected:
+    int _width;
+    int _height;
+    ofColor _color;
+};
+
+
 class testApp : public ofBaseApp{
 
 	public:
@@ -18,5 +87,19 @@ class testApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
+    
+//    
+//    void onPacket(const BufferedSerialEventArgs& packet);
+//    
+//    BufferedSerial serial;
+//    ByteBuffer buffer;
+    
+    std::vector<LED> localLEDs;
+    std::vector<LED> remoteLEDs;
+    
+    enum
+    {
+        DEFAULT_NUM_LEDS = 512
+    };
 		
 };
